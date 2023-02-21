6081aac910571fac09319f98b0a848afc459527f
Rudy100You <rudy.axel2000@gmail.com>
2023-02-21 18:25:54 -0300

    Initial Structure commit

---
+++ src/main/java/com/rsarabia/crmwebapp/CrmwebappApplication.java
@@ -0,0 +1,13 @@
+package com.rsarabia.crmwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmwebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rsarabia/crmwebapp/CrmwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsarabia.crmwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
